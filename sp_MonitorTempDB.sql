3727416fd43909816d7db62405df5ae82a79d3a9
Ben-DataPaws <167247129+Ben-DataPaws@users.noreply.github.com>
2026-02-16 14:52:24 -0500

    Create sp_MonitorTempDB.sql

---
+++ sp_MonitorTempDB.sql
@@ -0,0 +1,12 @@
+CREATE OR ALTER PROCEDURE [dbo].[sp_MonitorTempDB]
+(
+	@CheckConfiguration BIT = 0,
+	@DefaultAlerting BIT = 1,
+	@Debug BIT = 0
+)
+AS
+BEGIN
+/*
+sp_MonitorTempDB By DataPaws
+*/
+END
